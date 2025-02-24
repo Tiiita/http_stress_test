d603b52a74c4bc049448bdc9125247efa2dfd506
Tiiita <tiiitamail@gmail.com>
2025-02-24 18:32:49 +0100

    Add logging to file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::{
-    collections::HashMap, io::Write, process::exit, str::FromStr, sync::{
+    collections::HashMap,
+    fs::{self, OpenOptions},
+    io::Write,
+    process::exit,
+    str::FromStr,
+    sync::{
         atomic::{AtomicUsize, Ordering},
         Arc,
-    }, thread, time::{Duration, Instant}
+    },
+    thread,
+    time::{Duration, Instant},
 };
 
+use chrono::Local;
 use clap::{arg, command, Parser, ValueEnum};
 use colored::{ColoredString, Colorize};
 use reqwest::{Body, Client, Method, Request, Url};
@@ -31,6 +39,9 @@ struct Args {
     #[arg(short, long = "expected", default_value_t = 200)]
     expected_code: u16,
 
+    #[arg(short, default_value_t = false)]
+    logs: bool,
+
     #[arg(short = 'H', long)]
     headers: Option<Vec<String>>,
 }
@@ -43,6 +54,8 @@ async fn main() {
     let mut tasks = vec![];
     let timer_seconds = 3;
     let client = Arc::new(Client::new());
+
+    clear_log(args.logs);
 
     let request = build_request(args.clone(), prefix.clone());
 
@@ -75,46 +88,96 @@ async fn main() {
         let fails = Arc::clone(&fails);
         let client = Arc::clone(&client);
         let request = build_request(args.clone(), prefix.clone());
-        
+
         if args.delay != 0 {
             time::sleep(Duration::from_millis(args.delay.into())).await;
         }
-   
+
         tasks.push(task::spawn(async move {
             match client.execute(request).await {
                 Ok(response) => {
                     if response.status().as_u16() != args.expected_code {
                         fails.fetch_add(1, Ordering::Relaxed);
-                        println!("{prefix} Unexpected Status: {}", response.status().to_string().red());
+                        log_to_file(
+                            LogLevel::Error(
+                                format!(
+                                    "Got Unexpected Code (Expected: {}): {}",
+                                    args.expected_code,
+                                    response.status()
+                                )
+                                .as_str(),
+                            ),
+                            args.logs,
+                        );
+                        println!(
+                            "{prefix} Unexpected Status: {}",
+                            response.status().to_string().red()
+                        );
                     } else {
                         successes.fetch_add(1, Ordering::Relaxed);
+                        log_to_file(
+                            LogLevel::Info(format!("Got Response: {}", response.status()).as_str()),
+                            args.logs,
+                        );
                     }
                 }
                 Err(why) => {
                     fails.fetch_add(1, Ordering::Relaxed);
                     println!("{prefix} Request failed: {}", why.to_string().red());
+                    log_to_file(
+                        LogLevel::Error(
+                            format!("Sending request failed: {}", why.to_string()).as_str(),
+                        ),
+                        args.logs,
+                    );
                 }
             }
         }));
     }
 
-    for task in tasks {       
+    for task in tasks {
         task.await.unwrap();
     }
 
+    let elapsed_time = start_time.elapsed().as_millis();
+    let fails = fails.load(Ordering::Relaxed).to_string();
+    let successes = successes.load(Ordering::Relaxed).to_string();
     println!(
-        "{prefix} Done ({})! Successes: {}, Fails: {}",
-        format!("{} ms", start_time.elapsed().as_millis()).blue(),
-        successes.load(Ordering::Relaxed).to_string().green(),
-        fails.load(Ordering::Relaxed).to_string().red()
+        "{prefix} Done ({} ms)! Successes: {}, Fails: {}",
+        elapsed_time.to_string().blue(),
+        successes.to_string().green(),
+        fails.to_string().red()
+    );
+
+    log_to_file(
+        LogLevel::Info(
+            format!(
+                "Done ({} ms)! Successes: {}, Fails: {}",
+                elapsed_time, successes, fails
+            )
+            .as_str(),
+        ),
+        args.logs,
     );
 }
 
+enum LogLevel<'a> {
+    Info(&'a str),
+    Error(&'a str),
+}
+
+impl ToString for LogLevel<'_> {
+    fn to_string(&self) -> String {
+        match self {
+            LogLevel::Info(_) => "INFO".into(),
+            LogLevel::Error(_) => "ERROR".into(),
+        }
+    }
+}
 
 fn build_request(args: Args, prefix: ColoredString) -> Request {
     let headers_map: HashMap<String, String> = match args.headers {
-        Some(headers) => {
-            headers
+        Some(headers) => headers
             .iter()
             .map(|header| {
                 let parts: Vec<&str> = header.trim().split(":").collect();
@@ -126,9 +189,8 @@ fn build_request(args: Args, prefix: ColoredString) -> Request {
                 let value = parts[1].trim().to_string();
                 (key, value)
             })
-            .collect()
-        },
-        None => { HashMap::new() },
+            .collect(),
+        None => HashMap::new(),
     };
 
     let method = Method::from(args.method);
@@ -202,6 +264,50 @@ impl From<HttpMethod> for Method {
             HttpMethod::PATCH => Method::PATCH,
             HttpMethod::HEAD => Method::HEAD,
             HttpMethod::OPTIONS => Method::OPTIONS,
+        }
+    }
+}
+
+const LOG_FILE: &str = "http_stress_test.log";
+
+fn clear_log(enabled: bool) {
+    if enabled {
+        if let Err(why) = fs::write(LOG_FILE, "") {
+            eprintln!("Failed to clear log file: {}", why.to_string().red());
+        }
+    }
+}
+
+fn log_to_file(log: LogLevel, enabled: bool) {
+    if enabled {
+        let mut entry = String::new();
+        let now = Local::now();
+        let formatted_time = now.format("%Y-%m-%d %H:%M:%S%.3f").to_string();
+        match OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(LOG_FILE)
+        {
+            Ok(mut file) => {
+                match log {
+                    LogLevel::Info(msg) => entry.push_str(
+                        format!("[{} {}] {}\n", formatted_time, log.to_string(), msg).as_str(),
+                    ),
+                    LogLevel::Error(msg) => entry.push_str(
+                        format!("[{} {}] {}\n", formatted_time, log.to_string(), msg).as_str(),
+                    ),
+                }
+
+                let mut buf = entry.as_bytes();
+                if let Err(why) = file.write_all(&mut buf) {
+                    eprintln!("Failed to write to log file: {}", why.to_string().red())
+                }
+            }
+            Err(why) => {
+                eprintln!("Failed to open log file: {}", why.to_string().red())
+            }
         }
     }
 }
